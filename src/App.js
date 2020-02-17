96a65b13bb094954095025da73ae68dc5eb083d9
Ilia Feldgun <iliafeldgun@outlook.com>
2020-02-17 17:00:09 +0200

    Moved map component away

--- src/App.js
+++ src/App.js
@@ -1,54 +1,15 @@
 import React from 'react';
 import './App.css';
-import { ReactBingmaps } from 'react-bingmaps';
-
 import {MarkersManagementList} from './MarkerComponents';
+import {Map} from './MapComponents';
 
 const config = require('./config.json')
 
-
-
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      locationList : []
-    }
-    this.AddPushPinOnClick = this.AddPushPinOnClick.bind(this);
-
-  }
-  
-  AddPushPinOnClick(location)
-  {
-    let newLocationList = this.state.locationList.slice();
-    newLocationList.push(
-      {
-        latitude: location.latitude,
-        longtitude: location.longtitude,
-      }
-    )
-    this.setState({
-      locationList: newLocationList
-    })
-
-    console.log(newLocationList);
-  }
-  
   render(){
-    return (
-      <div id="bingMapsApp">
-      <div>
-        <MarkersManagementList markerList={this.state.locationList}></MarkersManagementList>
-      </div>
-      <ReactBingmaps 
-        bingmapKey = {config.dev.bing_maps_key} 
-        getLocation = {
-          {addHandler: "click", callback:this.AddPushPinOnClick}
-        }> 
-      </ReactBingmaps>
-      </div>
-    );
+    return (<Map></Map>)
   }
 }
+
 
 export default App;
