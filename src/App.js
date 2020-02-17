8c723c89996afb96f033b5fca90e9a30df972232
Ilia Feldgun <iliafeldgun@outlook.com>
2020-02-17 18:25:08 +0200

    Added form handling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
 import React from 'react';
 import './App.css';
-import {MarkersManagementList} from './MarkerComponents';
-import {Map} from './MapComponents';
 
-const config = require('./config.json')
+import {Map} from './MapComponents';
+import {MarkersForm} from './FormComponents';
+
+
 
 class App extends React.Component {
-  render(){
-    return (<Map></Map>)
+  constructor(props) {
+    super(props);
+    this.state = {
+      markerList : []
+    }
+    
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
+  
+  handleSubmit(e)
+  {
+      e.preventDefault();
+      let newMarkerList = this.state.markerList.slice();
+      newMarkerList.push(
+          {
+              longtitude: e.target.longtitude.value,
+              latitude: e.target.latitude.value
+
+
+          }
+      );
+      this.setState(
+          {
+              markerList: newMarkerList
+          }
+      )
+
+      console.log(this.state)
+  }
+  
+  render(){
+    return (
+    <div>
+      <div>
+        <MarkersForm markerList={this.state.markerList} handleSubmit={this.handleSubmit}/>
+      </div>
+      <div>
+        <Map markerList={[]/*this.state.markerList*/}/>
+      </div>
+    </div>
+    )
+  }
+
 }
 
 
--- src/FormComponents.js
+++ src/FormComponents.js
@@ -1,27 +1,16 @@
 import React from 'react';
 
 export class MarkersForm extends React.Component{
-    constructor(props){
-        super(props)
-        {  
-            this.handleSubmit = this.handleSubmit.bind(this)
-        }
-    }
-
-    handleSubmit(e)
-    {
-
-    }
     render(){
         return(
-            <form onSubmit={this.handleSubmit}>
+            <form onSubmit={this.props.handleSubmit}>
                 <label>longtitude:
                     <input name="longtitude" type="text" />
                 </label>
                 <label>latitude:
                     <input name="latitude" type="text" />
                 </label>
-                <input value="submit" type="submit" /> 
+                <input value="submit" type="Submit" /> 
             </form>
         )
     }
