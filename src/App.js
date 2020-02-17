3dc8a4ad362430b7b450cf0811c4247ea2b72cc2
Ilia Feldgun <iliafeldgun@outlook.com>
2020-02-17 15:50:42 +0200

    Experimentation with location pins

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { ReactBingmaps } from 'react-bingmaps';
 
 const config = require('./config.json')
 
-function App() {
-  return (
-    <ReactBingmaps 
-  bingmapKey = {config.dev.bing_maps_key} > 
-</ReactBingmaps>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.AddPushPinOnClick = this.AddPushPinOnClick.bind(this);
+
+  }
+  
+  AddPushPinOnClick(location)
+  {
+    console.log(location);
+  }
+  render(){
+    return (
+      <ReactBingmaps 
+        bingmapKey = {config.dev.bing_maps_key} 
+        getLocation = {
+          {addHandler: "click", callback:this.AddPushPinOnClick}
+        }> 
+      </ReactBingmaps>
+    );
+  }
 }
 
 export default App;
